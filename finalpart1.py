fbd47997bb7fad77f8fb940015c5e6d6cdc8a816
KevinHendersonCSUF <123698269+KevinHendersonCSUF@users.noreply.github.com>
2024-12-01 02:45:51 +0000

    NOT DONE

--- finalpart1.py
+++ finalpart1.py
@@ -1,2 +1,15 @@
 #Theo Gonong, Kevin Henderson, Anthony Le
 #Final Project part 1
+
+with open("final.txt", 'r') as fone:
+    with open("final24.txt", 'w') as ftwo:
+       for line in fone.readlines():
+        # if "(*" not in line and "*)" not in line and line.isspace() == False:
+        #     line = ' '.join(line.split())
+        #     format_line = line.strip()
+        #     ftwo.write(format_line + '\n')
+        if line.isspace() == False:
+            comment = "(*" 
+            line = ' '.join(line.split())
+            format_line = line.strip()
+            ftwo.write(format_line.split(comment)[0] + '\n')
