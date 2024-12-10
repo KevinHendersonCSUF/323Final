08f5054af1de6bfbccbcadcf1a6e54121f55c198
KevinHendersonCSUF <123698269+KevinHendersonCSUF@users.noreply.github.com>
2024-12-10 23:17:35 +0000

    added encoding=utf8 to with loop

--- finalpart1.py
+++ finalpart1.py
@@ -1,7 +1,7 @@
 #Theo Gonong, Kevin Henderson, Anthony Le
 #Final Project part 1
 
-with open("final.txt", 'r') as fone:
+with open("final.txt", 'r', encoding="utf8") as fone:
     with open("final24.txt", 'w') as ftwo:
        for line in fone.readlines():
         if "(*" not in line and "*)" not in line and line.isspace() == False:
