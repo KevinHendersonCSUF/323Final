1d6c89833e1411c8f3195f786b4c82c6b0887c2b
KevinHendersonCSUF <123698269+KevinHendersonCSUF@users.noreply.github.com>
2024-12-11 03:18:01 +0000

    added part 2

---
+++ finalpart2.py
@@ -0,0 +1,9 @@
+# Translated program in Python
+a = 3
+b2a = 14
+c = 5
+print(c)  # Print the value of c
+
+# Compute the expression
+bba = (b2a + 2 * c) * a
+print(f"value={bba}")  # Print the value of bba
